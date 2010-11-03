7db27e0420fe10d2b0fac3bd969a9b07c0e83972
eyedol <addhen@linuxmail.org>
2010-11-03 14:24:00 +0000

    Added page ping

--- kwemor.py
+++ kwemor.py
@@ -1,29 +1,76 @@
+from oauth import oauth
+from oauthtwitter import OAuthApi
+
+import pprint
+import httplib
+import socket
+import re
+
 import sys,os
 
-class Kwemore:
-    
+
+class Kwemor:
+    consumer_key = "yyx9XQKu3MjeudFnYbSMA"
+    consumer_secrete = "pmH6FvKZptKjJ2tytY0OunzDvGfDPD0GHG0YNdU"
+    oauth_token = "7415812-lTWSLvH3fTFC2lhWAtYJObBIiNze2yYvlBg3PQ9G8a"
+    oauth_token_secret = "QTeyNirnXhwcZJuXYEIUocK9uGZjBc47ynQ7rWLIpwQ"
+    oauth_verifier = "9822792"
+    twitter = ""
+
     def __init__(self): # constructor
         print "Kwemore, the website monitor"
-        
+        self.authenticate_app()
+    
+    #authenticate user
+    def authenticate_app(self):
+
+        self.twitter = OAuthApi(self.consumer_key, self.consumer_secrete,
+        self.oauth_token, self.oauth_token_secret)
+        user_timeline = self.twitter.GetUserTimeline();
+        #pp = pprint.PrettyPrinter(indent=4)
+        #pp.pprint(user_timeline)
     # print how to use kwemor from the commandline
-    def Usage(self):
-        print 'Usage: Kwemore \n\tpython kwemore.py --<arguments>\n\n\tArguments'
-        print '\t\t--adduser:\n\t\tuse this option to a new user.'
-        print '\n\t\te.g: python kwemore.py --adduser <twitter_username>\n\n' 
-        
+    def usage(self):
+        print 'Usage: Kwemor \n\tpython kwemor.py --<arguments>\n\n\tArguments'
+        print '\t\t--adduser:\n\t\tuse this option to add a new user.'
+        print '\n\t\te.g: python kwemore.py --adduser <twitter_username>\n\n'
+        print '\t\t--auth:\n\t\tuse this option to authenticate the kwemor app.'
+        print '\n\t\te.g: python kwemor.py --auth \n\n' 
+
     
-    def StrippedLine(self, line):
-        array = line.split("=>")
-        line = array[1].strip().strip("',").strip()
-        
-        if line.endswith(":"): line = line.rstrip(":")
-        if line.endswith("."): line = line.rstrip(".")
-        if '\\' in line: line = line.replace("\\", "")
-        
-        return line
+    # send DM to users 
+    def post_dm(self):
+        text = 'Your website is so up Mr.'
+        for user in self.read_lines('users.txt'):
+            if not user == "":
+                dm = self.twitter.SendDM(user,text)
+
+        # user = 'lorenzocabrini'
+        # text = 'Your website is so up Mr.'
+        # dm = self.twitter.SendDM(user,text)
+        # pp = pprint.PrettyPrinter(indent=4)
+        # pp.pprint(dm)
     
+    # send http get request to see if a website is up
+    def send_http_req(self,url, path='/'):
+        try:
+            conn = httplib.HTTPConnection(url)
+            conn.request("HEAD", path)
+            if re.match("^[23]\d\d$", str(conn.getresponse().status)):
+                return True
+        except StandardError:
+            return None
+    
+    def is_website_online(host):
+        try:
+            socket.gethostbyname(host)
+        except socket.gaierror:
+            return False
+        else:
+            return True
+
     # Read content of a file
-    def ReadLines(self, path):
+    def read_lines(self, path):
         f = open(path, 'r')
         content = f.readlines()
         
@@ -32,144 +79,42 @@ class Kwemore:
         return content
     
     #write content to a file
-    def WriteLines(self, path, content):
-        # by default we delete the file before we write anything to it
-        if os.path.exists(path): 
-            os.remove(path)
-            
+    def write_lines(self, content):
+        path = 'users.txt'    
         # recreate a new file and append the content to it
-        f = open(path, 'w')
-        f.writelines(content)
-        f.close()
-    
-        
-    def Unlisted(self, list = [], line = ""):
-        result = False
-        line = line.lower()
-        if line != "" and not line in list:
-            list.append(line)
-            result = True
-        
-        return result
-    
-    
-    def GeneratePot(self, dir, pfile):
-        temp = [ ]
-        list = [ ]
-        
-        for root, dirs, files in os.walk(dir):
-            for file in [f for f in files if f.endswith(".php")]:
-                lines = self.ReadLines(os.path.join(root, file))
-                
-                for line in [l for l in lines if "=>" in l and not l.strip().endswith('array')]:
-                    
-                    line = self.StrippedLine(line)
-
-                    if self.Unlisted(temp, line):
-                        list.append(line)
-        count = 0
-        list.sort() # just the default list sorting algorithm 
-        
-        if os.path.exists(pfile): os.remove(pfile)
-        f = open(pfile, 'w')
-        for msgid in list:
-            f.write(' Key:\n')
-            f.write('msgid "%s"\n'%msgid)
-            f.write('msgstr ""\n\n')
-            count += 1
+        f = open(path, 'a')
+        f.write(content)
         f.close()
         
-        return count
+    #add a new user
+    def add_user(self,username):
+        self.write_lines(username)
 
+        print "User %s" % username + " added."
 
-    def GeneratePHP(self, pfile, dir):
-        temp = [ ]
-        for line in self.ReadLines(pfile):
-            if not line == "":
-                temp.append(line)
-        count = 0
-        for root, dirs, files in os.walk(dir):
-            for file in [f for f in files if f.endswith(".php")]:
-                fn = os.path.join(root, file) # filename
-                list = [ ]
-                
-                for line in self.ReadLines(fn):
-                    if "=>" in line and not line.strip().endswith('array'):
-                        item = self.StrippedLine(line)
-                        msgid = ""
-                        msgstr = ""
-                        found = False
-                        
-                        for i in range(0, len(temp) - 1):
-                            if temp[i].startswith("msgid"):
-                                msgid  = temp[i].replace("msgid", "").strip().strip('"').strip()
-                                msgstr = msgid # just incase the translation isnt available, retain original string
-                                
-                                if msgid.lower() == item.lower():
-                                    found = True
-                                    msgstr = temp[i+1].replace("msgstr", "").strip().strip('"').strip()
-                                    break
-                        
-                        if found and msgid != "" and msgid != msgstr and msgid in line:
-                            if msgid.lower() in line.lower():
-                                if msgid.isupper(): # retain the original case
-                                    msgstr = msgstr.upper()
-                                line = line.replace(msgid, msgstr)
-                                list.append(line)
-                            count += 1
-                    else:
-                        list.append(line)
-                        
-                self.WriteLines(fn, list)       
-        return count
-
-    
-    def SyncFiles(self, oldfile, newfile):
-        list = [ ]
-        ol = self.ReadLines(oldfile) # old list
-        nl = self.ReadLines(newfile) # new list
-        
-        count = 0
-        for i in range(len(nl) - 1):
-            l = nl[i]
-            if l.startswith("msgid"):
-                msgid  = nl[i]
-                msgstr = nl[i+1]
-
-                for j in range(len(ol) - 1):
-                    if ol[j] == msgid:
-                        msgstr = ol[j+1]
-                        count += 1
-                        break
-                msgstr += '\n\n'
-                list.append(msgid)
-                list.append(msgstr)
-        
-        self.WriteLines(newfile, list)
-        return count
- 
+    def remove_user(self):
+        content = self.read_lines('users.txt')
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(content)
 
 if __name__ == '__main__':
-    i18n = Ushahidi18nParser()
+    kwemor = Kwemor()
     
     x = len(sys.argv)
     
     if x < 3:
-        i18n.Usage()
+        kwemor.usage()
     
     else:
-        if sys.argv[1] == '--genpot': # --genpot path potfile
-            print '\tGenerated a pot file with %s strings\n'%i18n.GeneratePot(sys.argv[2], sys.argv[3]) 
+        if sys.argv[1] == '--adduser': # --add a new user
+            kwemor.add_user(sys.argv[2]) 
    
-        elif sys.argv[1] == '--genphp': # --genphp potfile path
-            print '\tRegenerated the php files with %s translated strings\n'%i18n.GeneratePHP(sys.argv[2], sys.argv[3])
-          
-        elif sys.argv[1] == '--sync': # --sync oldfile newfile
-            print '\tSynced %s strings\n'%i18n.SyncFiles(sys.argv[2], sys.argv[3])
+        elif sys.argv[1] == '--dm': # --post a dm
+            kwemor.post_dm()
 
-        elif sys.argv[1] == '--usage': # --usage available options
-            i18n.Usage()
+        elif sys.arg[1] == '--rmuser': # --remove an user
+            kwemor.remove_user()
 
         else:
             print "Error: Unknown argument\n"
-            i18n.Usage()
+            kwemor.usage()
